461da3f508fcef512cf5aeeec1a3df57092dcf74
antran1245 <antran1245@gmail.com>
2022-05-12 12:55:58 -0700

    move front end to client

--- backend/server/routes/author.routes.js
+++ backend/server/routes/author.routes.js
@@ -1,6 +1,5 @@
-const express = require('express');
-const cors = require('cors');
-const app = express();
-require('./server/config/mongoose.config');
-app.use(cors());
-require('./server/routes/author.routes')(app)
+const AuthorController = require('../controllers/author.controller');
+
+module.exports = (app) => {
+    
+}
