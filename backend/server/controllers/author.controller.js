5fb4b2b018fbbe2f10576e031c83098db63ad02e
antran1245 <antran1245@gmail.com>
2022-05-12 00:04:35 -0700

    finish writing the controllers in backend

--- backend/server/controllers/author.controller.js
+++ backend/server/controllers/author.controller.js
@@ -15,3 +15,28 @@ module.exports.createAuthor = (req, res) => {
     .catch(err => res.json(err))
 }
 
+module.exports.findAllAuthor = (req, res) => {
+    Author.find()
+    .then(allAuthors => res.json(allAuthors))
+    .catch(err => res.json(err))
+}
+
+module.exports.findOneAuthor = (req, res) => {
+    Author.find({_id: req.params.id})
+    .then(author => res.json(author))
+    .catch(err => res.json(err))
+}
+
+module.exports.updateAuthor = (req, res) => {
+    Author.findOneAndUpdate(
+        {_id: req.params.id},
+        req.body,
+        {new: true, runValidators: true}
+    )
+}
+
+module.exports.deleteOneAuthor = (req, res) => {
+    Author.deleteOne({_id:req.params.id})
+    .then(deleteOne => res.json(deleteOne))
+    .catch(err => res.json(err))
+}
