51fb1c7047c947e40dcf5063e8e026447f04801e
antran1245 <antran1245@gmail.com>
2022-05-14 19:08:13 -0700

    remove runValidator

--- backend/server/controllers/author.controller.js
+++ backend/server/controllers/author.controller.js
@@ -10,8 +10,7 @@ module.exports.createAuthor = (req, res) => {
     const { name } = req.body;
     Author.create({
         name
-    },
-    {runValidators: true})
+    }
     .then(author => res.json(author))
     .catch(err => res.status(400).json(err))
 }
